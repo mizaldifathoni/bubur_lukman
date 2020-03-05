41fb8ea9b4e8eac2036df31fc90374c8a925d6e0
mizaldifathoni <mizaldifathoni@users.noreply.github.com>
2020-03-05 11:51:20 +0700

    Front-End Bubur Lukman

---
+++ application/views/Buburlukman/Home.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+    <title>Bubur Lukman</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/bootstrap.min.css">    
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/style.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/custom.css">
+
+</head>
+
+<body>
+	<!-- Start header -->
+	<header class="top-navbar">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img src="https://lh3.googleusercontent.com/proxy/VOAuLEW8_9z49Fwt9KbKxWcKcEz0ZGVMzPeLqlA8rKSLE22Q46-CqpbyxWcsax64VnAhqiCV5s-217obkwmHEOAgXtm6tJE_MaYI1tHZwHWA2g"   width="100px" height="100px" />
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbars-rs-food">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">About</a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="#">Reservation</a>
+								<a class="dropdown-item" href="#">Gallery</a>
+							</div>
+						</li>
+						<li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Admin</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+	
+	<!-- Start slides -->
+	<div id="slides" class="cover-slides">
+		<ul class="slides-container">
+			<li class="text-center">
+				<img src="https://www.free-css.com/assets/files/free-css-templates/preview/page249/yamifood/assets/images/slider-01.jpg" alt="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> Bubur Lukman</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="text-center">
+				<img src="https://www.free-css.com/assets/files/free-css-templates/preview/page249/yamifood/assets/images/slider-02.jpg" alt="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> Bubur Lukman</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="text-center">
+				<img src="https://i.pinimg.com/originals/ef/d2/d9/efd2d9b2f7f77d0483a01225d60e7ae2.jpg" alt="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> Bubur Lukman</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div class="slides-navigation">
+			<a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			<a href="#" class="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+		</div>
+	</div>
+	<!-- End slides -->
+	
+	<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<img src="https://lh3.googleusercontent.com/Z3Ku3BTXT6LwhB1-mG01cGHS3t0wuhyCYxjy9YtKTBbIDeswLsUTaFGXJY9qvnEJhoPfbyd-IIxTeJSg=w768-h768-n-o-v1" alt="" class="img-thumbnail" width="1000px" height="1000px">
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="inner-column">
+						<h1>Welcome To <span>Bubur Lukman</span></h1>
+						<h4>Little Story</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor suscipit feugiat. Ut at pellentesque ante, sed convallis arcu. Nullam facilisis, eros in eleifend luctus, odio ante sodales augue, eget lacinia lectus erat et sem. </p>
+						<p>Sed semper orci sit amet porta placerat. Etiam quis finibus eros. Sed aliquam metus lorem, a pellentesque tellus pretium a. Nulla placerat elit in justo vestibulum, et maximus sem pulvinar.</p>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+	
+	<!-- Start QT -->
+	<div class="qt-box qt-background">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 ml-auto mr-auto text-left">
+					<p class="lead ">
+						" If you're not the one cooking, stay out of the way and compliment the chef. "
+					</p>
+					<span class="lead">Michael Strahan</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End QT -->
+	
+	<!-- Start Menu -->
+	<div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Special Menu</h2>
+						<p>Belajarlah dari zombie, walaupun dia makan orang tapi dia gak pernah makan temen.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="special-menu text-center">
+						<div class="button-group filter-button-group">
+							<button class="active" data-filter="*">Semua</button>
+							<button data-filter=".drinks">Minuman</button>
+							<button data-filter=".lunch">Makanan</button>
+						</div>
+					</div>
+				</div>
+			</div>
+				
+			<div class="row special-list">
+				<div class="col-lg-4 col-md-6 special-grid drinks">
+					<div class="gallery-single fix">
+						<img src="https://www.free-css.com/assets/files/free-css-templates/preview/page249/yamifood/assets/images/img-01.jpg" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Teh Manis</h4>
+							<p>Bikin teh itu sama seperti buat janji, jangan terlalu MANIS.</p>
+							<h5> Rp 5000,-</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid drinks">
+					<div class="gallery-single fix">
+						<img src="https://www.free-css.com/assets/files/free-css-templates/preview/page249/yamifood/assets/images/img-02.jpg" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Jus</h4>
+							<p>Jus apa yang paling gak enak ? Jus a friend.</p>
+							<h5> Rp 8.000,-</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid drinks">
+					<div class="gallery-single fix">
+						<img src="https://nyaribisnis.com/wp-content/uploads/2019/10/Cara-Membuat-Americano-Sendiri.jpg" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Kopi</h4>
+							<p>Kopi apa yang paling pahit ? Kopilih dia daripada aku :(.</p>
+							<h5> Rp 5.000,-</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="https://lh3.googleusercontent.com/BrmaENFLxkRJL_jQSPBZKWfIp3g6ODW3fung8W6co-XyPg_4NTTs4w92_hXXlyDayweXXSXvQmGob5G7=w768-h768-n-o-v1" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Bubur Ayam</h4>
+							<p>Ketika nasi sudah menjadi bubur, tinggal tambahkan ayam, telur, kecap dan kerupuk.</p>
+							<h5> Rp 10.000,-</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="https://lh3.googleusercontent.com/uwFpeR2khj3xo0c9h1dIFCvEL4bw7sP-pfy28gMNB7usWB3rDbmCKUbUVkFKUSRFxJsap2cK5tWoCzBx=w768-h768-n-o-v1" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Bubur Kacang Hijau</h4>
+							<p>Awali pagimu dengan bubur kacang hijau karena kalau diawali dengan senyuman, jam 9 sudah lapar .</p>
+							<h5> Rp 8.000,-</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="https://lh3.googleusercontent.com/dbhMVntzyiYgv2OszWcjzrjjHb07qEpCYq4aHncuq2JlhwfT__YGjIR79VI7bmU1UmwUJX4sh5hD3cVk=w768-h768-n-o-v1" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Bubur Ketan Hitam</h4>
+							<p>Makan Bubur .</p>
+							<h5> Rp 8.000,-</h5>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 special-grid lunch">
+					<div class="gallery-single fix">
+						<img src="https://lh3.googleusercontent.com/DyVaIOLeCIYa3umt_riX53Wzou9PV0K__vZPL-D-oYJO0mPqprt9XofWOeJVZ8yd_i0HgMO2ObckkLHI=w768-h768-n-o-v1" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Bubur Campur</h4>
+							<p>Makan Bubur .</p>
+							<h5> Rp 8.000,-</h5>
+						</div>
+					</div>
+				</div>
+				
+				
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<!-- End Menu -->
+	
+	<!-- Start Gallery -->
+	<div class="gallery-box">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Gallery</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+					</div>
+				</div>
+			</div>
+			<div class="tz-gallery">
+				<div class="row">
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="https://scontent.fcgk3-2.fna.fbcdn.net/v/t1.0-9/s960x960/67631969_10206277026322337_5395795807993593856_o.jpg?_nc_cat=101&_nc_sid=8024bb&_nc_eui2=AeH-D0G_tB82FMiiZkYUG-RlZHAD3JQ-7fI2iFbusPvGrFzZ2wDpvWdjuws0Wi83jeBPh_YrGz2CPk7G3R7I7nCb4LYQhUnwxwMjJq4tgZVrBw&_nc_ohc=xHz2uqejooUAX87dE8u&_nc_ht=scontent.fcgk3-2.fna&_nc_tp=7&oh=d84336115e79e296bfb258e67d248fe5&oe=5E97239E">
+							<img class="img-fluid" src="https://scontent.fcgk3-2.fna.fbcdn.net/v/t1.0-9/s960x960/67631969_10206277026322337_5395795807993593856_o.jpg?_nc_cat=101&_nc_sid=8024bb&_nc_eui2=AeH-D0G_tB82FMiiZkYUG-RlZHAD3JQ-7fI2iFbusPvGrFzZ2wDpvWdjuws0Wi83jeBPh_YrGz2CPk7G3R7I7nCb4LYQhUnwxwMjJq4tgZVrBw&_nc_ohc=xHz2uqejooUAX87dE8u&_nc_ht=scontent.fcgk3-2.fna&_nc_tp=7&oh=d84336115e79e296bfb258e67d248fe5&oe=5E97239E" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="https://scontent.fcgk3-1.fna.fbcdn.net/v/t1.0-9/75116943_262468808047348_2007196319461933056_o.jpg?_nc_cat=104&_nc_sid=110474&_nc_eui2=AeGcI9Gv-lOVTsxEs60MpSzudB2g0ssuDgcShha_fgdc8UmKejI0vBsFSqjLdjV-P3OZylA5MpSjOQvR0epvHZNOUczF_jDHCZtsijEZFdJuTg&_nc_ohc=9RfzdvfmkUYAX-lkJp5&_nc_ht=scontent.fcgk3-1.fna&oh=87c7f8f08fe1af190824dedba493dc74&oe=5E9A041E">
+							<img class="img-fluid" src="https://scontent.fcgk3-1.fna.fbcdn.net/v/t1.0-9/75116943_262468808047348_2007196319461933056_o.jpg?_nc_cat=104&_nc_sid=110474&_nc_eui2=AeGcI9Gv-lOVTsxEs60MpSzudB2g0ssuDgcShha_fgdc8UmKejI0vBsFSqjLdjV-P3OZylA5MpSjOQvR0epvHZNOUczF_jDHCZtsijEZFdJuTg&_nc_ohc=9RfzdvfmkUYAX-lkJp5&_nc_ht=scontent.fcgk3-1.fna&oh=87c7f8f08fe1af190824dedba493dc74&oe=5E9A041E" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="https://scontent.fcgk3-2.fna.fbcdn.net/v/t1.0-9/s960x960/73472677_10206563127114678_8095925593893240832_o.jpg?_nc_cat=102&_nc_sid=8024bb&_nc_eui2=AeEykttnGowAxibWrO4-3PrMXNdSlfIuUuIwIc31_-L1TDzYDELvRRP1K-tWOvLqh_zEiNg_Q5ZvIpB5dVd2MayFI0GnOCEsKJl6-l5o6RBcmQ&_nc_ohc=2R_ZSIlH5isAX_m2vHG&_nc_ht=scontent.fcgk3-2.fna&_nc_tp=7&oh=a1bb226ebadf00539e31170451933669&oe=5E800D7B">
+							<img class="img-fluid" src="https://scontent.fcgk3-2.fna.fbcdn.net/v/t1.0-9/s960x960/73472677_10206563127114678_8095925593893240832_o.jpg?_nc_cat=102&_nc_sid=8024bb&_nc_eui2=AeEykttnGowAxibWrO4-3PrMXNdSlfIuUuIwIc31_-L1TDzYDELvRRP1K-tWOvLqh_zEiNg_Q5ZvIpB5dVd2MayFI0GnOCEsKJl6-l5o6RBcmQ&_nc_ohc=2R_ZSIlH5isAX_m2vHG&_nc_ht=scontent.fcgk3-2.fna&_nc_tp=7&oh=a1bb226ebadf00539e31170451933669&oe=5E800D7B" alt="Gallery Images">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Gallery -->
+
+	
+	<!-- Start Contact info -->
+	<div class="contact-imfo-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<i class="fa fa-volume-control-phone"></i>
+					<div class="overflow-hidden">
+						<h4>Phone</h4>
+						<p class="lead">
+							+628xxxxxxxxxx
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-envelope"></i>
+					<div class="overflow-hidden">
+						<h4>Email</h4>
+						<p class="lead">
+							buburlukman@gmail.com
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-map-marker"></i>
+					<div class="overflow-hidden">
+						<h4>Location</h4>
+						<p class="lead">
+							Jl. Pulau Legundi No.202
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact info -->
+	
+	<!-- Start Footer -->
+	<footer class="footer-area bg-f">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<h3>About Us</h3>
+					<p>Bubur Lukman berdiri sejak tahun 1997, dan didirikan oleh bapak abubakar yang mana bermula bapak abubakar yang menyukai bubur ayam kemudian beliau akhirnya membuat suatu usaha keluarga, yang mana nama usaha tersebut terinspirasi oleh nama anak pertama beliau.</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Opening hours</h3>
+					<p><span class="text-color">Senin-Minggu :</span> 05.30 WIB - 21.00 WIB</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Contact information</h3>
+					<p class="lead">Jl. Pulau Legundi No.202</p>
+					<p class="lead"><a href="#">+62xxxxxxxxxxx</a></p>
+					<p><a href="#"> info@admin.com</a></p>
+					<ul class="list-inline f-social">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<p class="company-name">All Rights Reserved. &copy; 2020 <a href="#">Bubur Lukman</a> Design By : 
+					<a href="https://html.design/">AMAN</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+	<!-- ALL JS FILES -->
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/popper.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/jquery.superslides.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/images-loded.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/isotope.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/baguetteBox.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/form-validator.min.js"></script>
+    <script src="<?php echo base_url('assets/assets_yamifood') ?>/js/contact-form-script.js"></script>
+    <script src="<?php echo base_url('assets/assets_yamifood') ?>/js/custom.js"></script>
+</body>
+</html>
