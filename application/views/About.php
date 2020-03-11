8be040a0fe08624bfc051fb8f198bb88a6a9c07f
mizaldifathoni <mizaldifathoni@users.noreply.github.com>
2020-03-11 14:42:13 +0700

    menu about

---
+++ application/views/About.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+    <title>Bubur Lukman</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/bootstrap.min.css">    
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/style.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/assets_yamifood') ?>/css/custom.css">
+
+</head>
+
+<body>
+	<!-- Start header -->
+	<header class="top-navbar">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img src="assets/assets_yamifood/images/bubur.jpg" width="60px" height="60px" />
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbars-rs-food">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active"><a class="nav-link" href="<?php echo base_url('Home') ?> ">Home</a></li>
+						<li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+						<li class="nav-item"><a class="nav-link" href="<?php echo base_url('About') ?> ">About</a></li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown-a">
+								<a class="dropdown-item" href="#">Reservation</a>
+								<a class="dropdown-item" href="#">Gallery</a>
+							</div>
+						</li>
+						<li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+	
+	<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					</br></br></br><img src="https://lh3.googleusercontent.com/Z3Ku3BTXT6LwhB1-mG01cGHS3t0wuhyCYxjy9YtKTBbIDeswLsUTaFGXJY9qvnEJhoPfbyd-IIxTeJSg=w768-h768-n-o-v1" alt="" class="img-thumbnail" width="1000px" height="1000px">
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="inner-column">
+						</br></br></br><h1>Bubur Lukman</h1>
+						<h2>Tentang Kami</h2>
+						<p>Bubur Lukman berdiri sejak tahun 1997, dan didirikan oleh bapak abubakar yang mana bermula bapak abubakar yang menyukai bubur ayam kemudian beliau akhirnya membuat suatu usaha keluarga, yang mana nama usaha tersebut terinspirasi oleh nama anak pertama beliau. </p>
+						<h4>Saat ini Bubur Lukman memiliki 3 outlet, yang terletak di : </h4>
+						<ul>
+							<li>Jl. Pulau Legundi No.202, Sukarame, Kota Bandar Lampung</li>
+							<li>Jl. Al Hikmah, Sukabumi Indah, Sukabumi, Kota Bandar Lampung</li>
+							<li>Jl. Pulau Singkep, Sukarame, Kota Bandar Lampung</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+
+	
+	<!-- Start Footer -->
+	<footer class="footer-area bg-f">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<h3>Opening hours</h3>
+					<p><span class="text-color">Senin-Minggu :</span> 05.30 WIB - 21.00 WIB</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Contact information</h3>
+					<p class="lead"><a href="#">+62xxxxxxxxxxx</a></p>
+					<p><a href="#"> info@admin.com</a></p>
+					<ul class="list-inline f-social">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<p class="company-name">All Rights Reserved. &copy; 2020 <a href="#">Bubur Lukman</a> Design By : 
+					<a href="https://html.design/">AMAN</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+	<!-- ALL JS FILES -->
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/popper.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/jquery.superslides.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/images-loded.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/isotope.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/baguetteBox.min.js"></script>
+	<script src="<?php echo base_url('assets/assets_yamifood') ?>/js/form-validator.min.js"></script>
+    <script src="<?php echo base_url('assets/assets_yamifood') ?>/js/contact-form-script.js"></script>
+    <script src="<?php echo base_url('assets/assets_yamifood') ?>/js/custom.js"></script>
+</body>
+</html>
--- application/views/Home.php
+++ application/views/Home.php
@@ -35,16 +35,16 @@
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="#">
-					<img src="assets/assets_yamifood/images/bubur.jpg" width="90px" height="90px" />
+					<img src="assets/assets_yamifood/images/bubur.jpg" width="60px" height="60px" />
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
 				  <span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbars-rs-food">
 					<ul class="navbar-nav ml-auto">
-						<li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+						<li class="nav-item active"><a class="nav-link" href="<?php echo base_url('Home') ?> ">Home</a></li>
 						<li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
-						<li class="nav-item"><a class="nav-link" href="#">About</a></li>
+						<li class="nav-item"><a class="nav-link" href="<?php echo base_url('About') ?> ">About</a></li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
