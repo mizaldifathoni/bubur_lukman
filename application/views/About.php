f3b0fff0c1ac3e3e3a4e89fe3ac160868051bbcc
mizaldifathoni <mizaldifathoni@users.noreply.github.com>
2020-03-14 18:33:27 +0700

    .

--- application/views/About.php
+++ application/views/About.php
@@ -34,7 +34,7 @@
 	<header class="top-navbar">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
-				<a class="navbar-brand" href="#">
+				<a class="navbar-brand" href="<?php echo base_url('Home') ?>">
 					<img src="assets/assets_yamifood/images/bubur_lukman.png" width="60px" height="60px" />
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
